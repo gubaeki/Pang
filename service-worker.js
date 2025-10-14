b15618e78b6309d7082c83e2717b8379dacab845
gubaeki <110735099+gubaeki@users.noreply.github.com>
2025-10-14 23:48:45 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,16 @@
+const CACHE_NAME = 'baekhopang_V1'; // manifest.json 수정 시 버전정보 변경 필요
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('anipang-pwa-test-v1').then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         './',
         './index.html',
         './main.js',
         './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
+        './main.css',
+        './icon-192.png', // 아이콘 변경필요
+        './icon-512.png'  // 아이콘 변경필요
       ]);
     })
   );
